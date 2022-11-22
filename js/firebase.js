142a63e8f76872575f7d5cbc3759af834ca43db4
Manuel Aguilar <abyssal9808@gmail.com>
2022-11-22 14:53:00 -0700

    yes
    updated departamentos, clase y familia

--- js/firebase.js
+++ js/firebase.js
@@ -12,6 +12,7 @@ import {
   doc,
   getDoc,
   updateDoc,
+  setDoc
 } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -51,3 +52,69 @@ export const updateTask = (id, newFields) =>
 export const getTasks = () => 
   getDocs(collection(db, "Articulo"));
 
+// TABLAS:
+const DEPARTAMENTO = collection(db, 'Departamento');
+const CLASE = collection(db, 'Clase');
+const FAMILIA = collection(db, 'Familia');
+
+
+//---------------           DEPARTAMENTO           ---------------//
+
+
+export const saveDepto = (no, departamento) => 
+  setDoc(doc(DEPARTAMENTO, no ), 
+    {departamento} ); //guardar Documentos id
+ 
+export const verDepto = () => 
+  getDocs(DEPARTAMENTO);
+
+export const reRenderDepto = (callback) => 
+  onSnapshot(DEPARTAMENTO, callback); //renderizar en tiempo real
+
+export const eliminarDepto = (idDoc) => 
+  deleteDoc(doc(DEPARTAMENTO, idDoc)); //eliminar por id
+
+export const verDeptoDoc = () => 
+  getDoc(doc(DEPARTAMENTO)); //
+
+
+
+//---------------           CLASE           ---------------//
+
+
+export const saveClase = (no, clase) => 
+setDoc(doc(CLASE, no ), 
+  {clase} ); //guardar Documentos id
+
+export const verClase = () => 
+getDocs(CLASE);
+
+export const reRenderClase = (callback) => 
+onSnapshot(CLASE, callback); //renderizar en tiempo real
+
+export const eliminarClase = (idDoc) => 
+deleteDoc(doc(CLASE, idDoc)); //eliminar por id
+
+export const verClaseDoc = () => 
+getDoc(doc(CLASE)); //
+
+
+
+//---------------          FAMILIA          ---------------//
+
+
+export const saveFam = (no, familia) => 
+setDoc(doc(FAMILIA, no ), 
+  {familia} ); //guardar Documentos id
+
+export const verFamilia = () => 
+getDocs(FAMILIA);
+
+export const reRenderFamilia = (callback) => 
+onSnapshot(FAMILIA, callback); //renderizar en tiempo real
+
+export const eliminarFamilia = (idDoc) => 
+deleteDoc(doc(FAMILIA, idDoc)); //eliminar por id
+
+export const verFamiliaDoc = () => 
+getDoc(doc(FAMILIA)); //
--- js/nav.js
+++ js/nav.js
@@ -2,14 +2,23 @@
 const options = document.querySelector('.barra-tareas-options');
 
 const artOpt = document.getElementById('art-opt');
+const deptoOpt = document.getElementById('depto-opt');
+const claseOpt = document.getElementById('clase-opt');
+const familiaOpt = document.getElementById('familia-opt');
 const dbOpt = document.getElementById('db-opt');
 
 // items
 const artItem = document.querySelector('#art-item');
+const deptoItem = document.querySelector('#depto-item');
+const claseItem = document.querySelector('#clase-item');
+const familiaItem = document.querySelector('#familia-item');
 const dbItem = document.querySelector('#db-item');
 
 // forms
 const formArt = document.querySelector("#myform");
+const formDepto = document.querySelector("#myDepto");
+const formClase = document.querySelector("#myClase");
+const formFamilia = document.querySelector("#myFamilia");
 const formDb = document.querySelector("#myData");
 
 
@@ -20,6 +29,9 @@ const windowsIcon = document.querySelector('#windows-icon');
 // otros
 
 artItem.style.display = "flex";
+formDepto.style.display = "none";
+formClase.style.display = "none";
+formFamilia.style.display = "none";
 formDb.style.display = "none";
 
 // Desktop Button
@@ -33,6 +45,24 @@ artOpt.addEventListener('click', () => {
     artItem.style.display = 'flex';
     options.classList.remove('active');
 })
+
+deptoOpt.addEventListener('click', () => {
+    formDepto.style.display = 'flex';
+    deptoItem.style.display = 'flex';
+    options.classList.remove('active');
+});
+
+claseOpt.addEventListener('click', () => {
+    formClase.style.display = 'flex';
+    claseItem.style.display = 'flex';
+    options.classList.remove('active');
+});
+
+familiaOpt.addEventListener('click', () => {
+    formFamilia.style.display = 'flex';
+    familiaItem.style.display = 'flex';
+    options.classList.remove('active');
+});
 
 dbOpt.addEventListener('click', () => {
     formDb.style.display = 'flex';
@@ -49,11 +79,32 @@ artItem.addEventListener('click', () => {
 dbItem.addEventListener('click', () => {
     formDb.style.display = 'flex';
 })
+claseItem.addEventListener('click', () => {
+    formClase.style.display = 'flex';
+})
+familiaItem.addEventListener('click', () => {
+    formFamilia.style.display = 'flex';
+})
+deptoItem.addEventListener('click', () => {
+    formDepto.style.display = 'flex';
+})
 
 // minimizing
 const miniForm = document.querySelector('#minForm').addEventListener('click', () => {
     formArt.style.display = 'none';
     artItem.style.display = "flex";
+})
+const miniDepto = document.querySelector('#minDepto').addEventListener('click', () => {
+    formDepto.style.display = 'none';
+    deptoItem.style.display = "flex";
+})
+const miniClase = document.querySelector('#minClase').addEventListener('click', () => {
+    formClase.style.display = 'none';
+    claseItem.style.display = "flex";
+})
+const miniFamilia = document.querySelector('#minFamilia').addEventListener('click', () => {
+    formFamilia.style.display = 'none';
+    familiaItem.style.display = "flex";
 })
 const miniDb = document.querySelector('#minDb').addEventListener('click', () => {
     formDb.style.display = 'none';
@@ -80,7 +131,18 @@ const closeForm = document.querySelector('#clForm').addEventListener('click', ()
     formArt.style.display = 'none';
     artItem.style.display = 'none';
 })
-
+const closeDepto = document.querySelector('#clDepto').addEventListener('click', () => {
+    formDepto.style.display = 'none';
+    deptoItem.style.display = 'none';
+})
+const closeClase = document.querySelector('#clClase').addEventListener('click', () => {
+    formClase.style.display = 'none';
+    claseItem.style.display = 'none';
+})
+const closeFamilia = document.querySelector('#clFamilia').addEventListener('click', () => {
+    formFamilia.style.display = 'none';
+    familiaItem.style.display = 'none';
+})
 const closeDb = document.querySelector('#clDb').addEventListener('click', () => {
     formDb.style.display = 'none';
     dbItem.style.display = 'none';
